4d7c7f287412681af4eb96ab11e08d411fc65e3e
Jezz Santos <jezz.santos@hotmail.com>
2017-02-27 21:39:47 +1300

    Made the subscriptionStore configurable in the feature

--- src/Webhooks.UnitTests/WebhookFeatureSpec.cs
+++ src/Webhooks.UnitTests/WebhookFeatureSpec.cs
@@ -1,22 +1,85 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Funq;
+using Moq;
+using NUnit.Framework;
+using ServiceStack.Webhooks.ServiceInterface;
+using ServiceStack.Webhooks.ServiceModel.Types;
 
 namespace ServiceStack.Webhooks.UnitTests
 {
-    [TestFixture]
     public class WebhookFeatureSpec
     {
-        private WebhookFeature feature;
-
-        [SetUp]
-        public void InitializeContext()
+        [TestFixture]
+        public class GivenNoContext
         {
-            feature = new WebhookFeature();
+            [Test, Category("Unit")]
+            public void WhenRegister_ThenDependenciesRegistered()
+            {
+                var appHost = new Mock<IAppHost>();
+                var container = new Container();
+                appHost.As<IHasContainer>().Setup(ah => ah.Container)
+                    .Returns(container);
+
+                new WebhookFeature().Register(appHost.Object);
+
+                Assert.That(container.GetService(typeof(ICurrentCaller)), Is.TypeOf<AuthSessionCurrentCaller>());
+            }
+
+            [Test, Category("Unit")]
+            public void WhenRegisterAndSubscriptionStoreNotRegistered_ThenRegistersMemoryStoreByDefault()
+            {
+                var appHost = new Mock<IAppHost>();
+                var container = new Container();
+                appHost.As<IHasContainer>().Setup(ah => ah.Container)
+                    .Returns(container);
+
+                new WebhookFeature().Register(appHost.Object);
+
+                Assert.That(container.GetService(typeof(IWebhookSubscriptionStore)), Is.TypeOf<MemoryWebhookSubscriptionStore>());
+            }
+
+            [Test, Category("Unit")]
+            public void WhenRegisterAndSubscriptionStoreAlreadyRegistered_ThenDoesNotRegisterMemoryStoreByDefault()
+            {
+                var appHost = new Mock<IAppHost>();
+                var container = new Container();
+                container.Register<IWebhookSubscriptionStore>(new TestSubscriptionStore());
+                appHost.As<IHasContainer>().Setup(ah => ah.Container)
+                    .Returns(container);
+
+                new WebhookFeature().Register(appHost.Object);
+
+                Assert.That(container.GetService(typeof(IWebhookSubscriptionStore)), Is.TypeOf<TestSubscriptionStore>());
+            }
         }
 
-        [Test, Category("Unit")]
-        public void WhenCtor_Then()
+        public class TestSubscriptionStore : IWebhookSubscriptionStore
         {
-            Assert.That(true, Is.True);
+            public string Add(WebhookSubscription subscription)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<WebhookSubscription> Find(string userId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public WebhookSubscription Get(string userId, string eventName)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update(string subscriptionId, WebhookSubscription subscription)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(string subscriptionId)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
--- src/Webhooks/WebhookFeature.cs
+++ src/Webhooks/WebhookFeature.cs
@@ -1,4 +1,5 @@
-﻿using ServiceStack.Validation;
+﻿using Funq;
+using ServiceStack.Validation;
 using ServiceStack.Webhooks.ServiceInterface;
 using ServiceStack.Webhooks.ServiceModel;
 
@@ -9,6 +10,22 @@ namespace ServiceStack.Webhooks
         public void Register(IAppHost appHost)
         {
             var container = appHost.GetContainer();
+
+            RegisterSubscriptionStore(container);
+            RegisterSubscriptionService(appHost);
+        }
+
+        private static void RegisterSubscriptionStore(Container container)
+        {
+            if (!container.Exists<IWebhookSubscriptionStore>())
+            {
+                container.RegisterAutoWiredAs<MemoryWebhookSubscriptionStore, IWebhookSubscriptionStore>();
+            }
+        }
+
+        private static void RegisterSubscriptionService(IAppHost appHost)
+        {
+            var container = appHost.GetContainer();
             appHost.RegisterService(typeof(SubscriptionService));
 
             container.RegisterValidators(typeof(WebHookInterfaces).Assembly, typeof(SubscriptionService).Assembly);
@@ -16,7 +33,6 @@ namespace ServiceStack.Webhooks
             container.RegisterAutoWiredAs<SubscriptionConfigValidator, ISubscriptionConfigValidator>();
 
             container.RegisterAutoWiredAs<AuthSessionCurrentCaller, ICurrentCaller>();
-            container.RegisterAutoWiredAs<MemoryWebhookSubscriptionStore, IWebhookSubscriptionStore>();
         }
     }
 }

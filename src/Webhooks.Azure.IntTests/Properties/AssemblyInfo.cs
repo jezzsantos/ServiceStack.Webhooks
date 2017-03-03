d058e27b64c4836c0e6c0d1b16c3a51eb02e68d9
Jezz Santos <jezz.santos@hotmail.com>
2017-03-04 08:50:08 +1300

    Split out the ServiceStack.Webhooks.Azure repo (now at : http://github.com/jezzsantos/ServiceStack.Webhooks.Azure)

--- src/Webhooks.Azure.IntTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ServiceStack.Webhooks.Azure.IntTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("684af930-511a-4f19-998c-fbb6cf09b35b")]
--- src/Webhooks.Azure/AzureStorage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServiceStack.Webhooks.Azure
-{
-    public static class AzureStorage
-    {
-        internal const string AzureEmulatorConnectionString = @"UseDevelopmentStorage=true";
-    }
-}

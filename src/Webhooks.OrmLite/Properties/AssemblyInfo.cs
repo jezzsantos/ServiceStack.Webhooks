d746b47bd90e028213e79a107884c8e1d026992d
Jezz Santos <jezz.santos@hotmail.com>
2017-03-09 19:21:04 +1300

    Added missing nuspec description

--- src/Webhooks.OrmLite/Properties/AssemblyInfo.cs
+++ src/Webhooks.OrmLite/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ServiceStack.Webhooks.OrmLite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Add OrmLite plugins to ServiceStack Webhooks")]
 [assembly: Guid("26284e4c-d5bb-4e3a-aa24-312da9a468ab")]

e693dd16329697991cccaa876497d3ff6349360f
Jezz Santos <jezz.santos@hotmail.com>
2017-03-14 17:16:41 +1300

    Strongname signed all assemblies, fixing #10

--- src/Webhooks.Interfaces/Properties/AssemblyInfo.cs
+++ src/Webhooks.Interfaces/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ServiceStack.Webhooks.Interfaces")]
 [assembly: AssemblyDescription("Add Webhooks to your ServiceStack services")]
 [assembly: Guid("861cb055-15cb-4909-94a4-2a357138d39c")]
-[assembly: InternalsVisibleTo("ServiceStack.Webhooks.Interfaces.UnitTests")]
+[assembly: InternalsVisibleTo("ServiceStack.Webhooks.Interfaces.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010069e099f19bb6e78cf974bb5167e76a0d17c2bd653e791133b25e54ab63ac6ddcd0ecbd634e34a0efe9f5fff58e2b1180edc764d9f3c959252a37e91bba22ec2466d6412d8d4bbb8ccb7ef04224feb9fa55c1738821e90d3e7f61ee42db40af778f660bc1bff2902fa9789283c6d7fcb9faad1cfb61e0b4dfa44514243e4f61eb")]
--- src/Webhooks.Relays/Properties/AssemblyInfo.cs
+++ src/Webhooks.Relays/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ServiceStack.Webhooks.Relays")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("1e47701b-1779-4eee-8b32-8112c283d6f7")]
-[assembly: InternalsVisibleTo("ServiceStack.Webhooks.Relays.UnitTests")]
+[assembly: InternalsVisibleTo("ServiceStack.Webhooks.Relays.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010069e099f19bb6e78cf974bb5167e76a0d17c2bd653e791133b25e54ab63ac6ddcd0ecbd634e34a0efe9f5fff58e2b1180edc764d9f3c959252a37e91bba22ec2466d6412d8d4bbb8ccb7ef04224feb9fa55c1738821e90d3e7f61ee42db40af778f660bc1bff2902fa9789283c6d7fcb9faad1cfb61e0b4dfa44514243e4f61eb")]

26024baab626e0ffdbc418e27f3b7d61c70c4404
Jezz Santos <jezz.santos@hotmail.com>
2017-03-15 09:35:28 +1300

    1.7.0

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
-[assembly: AssemblyInformationalVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
 
 #if ASSEMBLYSIGNED
 [assembly:

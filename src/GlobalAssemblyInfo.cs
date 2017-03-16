8b8a32e4014ab304680bbd21988b766852019bb2
Jezz Santos <jezz.santos@hotmail.com>
2017-03-16 13:04:17 +1300

    1.9.0

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.7.1")]
-[assembly: AssemblyInformationalVersion("1.7.1")]
+[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
 
 #if ASSEMBLYSIGNED
 [assembly:

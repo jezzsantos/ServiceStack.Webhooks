bea09ea7ee6f1041889fe193747c966bb82c96f0
Jezz Santos <jezz.santos@hotmail.com>
2017-12-03 23:49:16 +1300

    1.10.5

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.10.4")]
-[assembly: AssemblyInformationalVersion("1.10.4")]
+[assembly: AssemblyFileVersion("1.10.5")]
+[assembly: AssemblyInformationalVersion("1.10.5")]
 
 #if ASSEMBLYSIGNED
 [assembly:

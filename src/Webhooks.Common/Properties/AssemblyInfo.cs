e0a204f762bc0813b006e4b8172ba2977be864ee
Jezz Santos <jezz.santos@hotmail.com>
2020-01-18 13:10:25 +1300

    3.0.1

--- src/Webhooks.Common/Properties/AssemblyInfo.cs
+++ src/Webhooks.Common/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ServiceStack.Webhooks.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Common types for working with webhooks")]
 [assembly: Guid("ebd7353a-7bcd-4b65-90b9-fdc9a6dca7fe")]
